00522a8b122ebcc51e5248cc78539732a2a6b410
Percy Maldonado Quispe <percy.maldonado@ucsp.edu.pe>
2017-08-31 10:11:25 -0500

    observer

---
+++ teoria/observer.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+class observer{
+public:
+	observer();
+	~observer();
+};
+
+class account{
+private:
+public:
+	account();
+};
+
+int main(int argc, char const *argv[]){
+	return 0;
+}
