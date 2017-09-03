1fef14b0f6a89c4aa5985a61cfd470e27c34bc5d
Percy Maldonado Quispe <percy.maldonado@ucsp.edu.pe>
2017-09-03 16:23:38 -0500

    interface

--- lab/Main.java
+++ lab/Main.java
@@ -1,3 +1,28 @@
+interface A{
+	public void a();
+}
+
+interface B extends A{
+	public void b();
+}
+
+interface C extends A{
+	public void c();
+}
+
+class D implements B,C{
+	public void a(){
+		System.out.println("A");
+	}
+	public void b(){
+		System.out.println("B");
+	}
+	public void c(){
+		System.out.println("C");
+	}
+}
+
+
 public class Main{
 	public static void main(String[] args){	
 		System.out.println("Percy");
